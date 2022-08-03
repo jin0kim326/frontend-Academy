73dcbd177d79fb257d137adc5c3b8b5e999c1a72
iamyewon <101702107+iamyewon@users.noreply.github.com>
2022-08-03 15:21:00 +0900

    8.3
    중간커밋

--- 프로그래밍언어응용_김예원/js/login.js
+++ 프로그래밍언어응용_김예원/js/login.js
@@ -1,48 +1,49 @@
-const userid = document.querySelector('#userid');
-const passwd = document.querySelector('#passwd');
-const loginBtn = document.querySelector('#login-btn');
-const USER_ID = 'yeni12345';
-const USER_PASSWD = 'yeni123123123'
+const userid = document.querySelector("#userid");
+const passwd = document.querySelector("#passwd");
+const loginBtn = document.querySelector("#login-btn");
+const errorId = document.querySelector(".error-id");
+const errorPasswd = document.querySelector(".error-passwd");
+const USER_ID = "yeni12345";
+const USER_PASSWD = "yeni123123123";
 
 checkId = () => {
-  if(userid.value === null || userid.value === ''){
-    alert('a');
+  if (userid.value === null || userid.value === "") {
+    errorId.innerText = "아이디를 입력하세요.";
     return false;
-  }else if(userid.value.length < 9){
-    alert('b')
+  } else if (userid.value.length < 9) {
+    errorId.innerText = "아이디를 9자리 이상 입력해주세요.";
     return false;
-  }else{
+  } else {
     return true;
   }
-}
+};
 
 checkPasswd = () => {
-  if(passwd.value === null || passwd.value === ''){
-    alert('c');
+  if (passwd.value === null || passwd.value === "") {
+    errorPasswd.innerText = "비밀번호를 입력하세요.";
     return false;
-  }else if(passwd.value.length < 12){
-    alert('d');
+  } else if (passwd.value.length < 12) {
+    errorPasswd.innerText = "비밀번호를 12자리 이상 입력해주세요.";
     return false;
-  }else{
+  } else {
     return true;
   }
-}
+};
 
-checkAuthUser = () => { 
-  if(userid.value === USER_ID && passwd.value === USER_PASSWD){
-    location.href = 'https://nid.naver.com/nidlogin.login';
+checkMember = () => {
+  if (userid.value === USER_ID && passwd.value === USER_PASSWD) {
+    location.href = "https://nid.naver.com/nidlogin.login";
   } else {
-    alert('없는 회원입니둥.');
+    document.querySelector(".login-error-message").innerText =
+      "아이디 혹은 비밀번호를 잘못 입력했습니다. \n 입력하신 내용을 다시 확인해주세요.";
   }
-}
+};
 
 checkLogin = (e) => {
   e.preventDefault();
   checkId();
   checkPasswd();
-  checkAuthUser();
-} 
-
+  checkMember();
+};
 
 // loginBtn.addEventListener("click", checkLogin);
-
--- 프로그래밍언어응용_김예원/js/register.js
+++ 프로그래밍언어응용_김예원/js/register.js
@@ -10,6 +10,8 @@ const tel = document.querySelector("#tel");
 const errorMsgTel = document.querySelector(".errorMsg-tel");
 const btn = document.querySelector("#signup-btn");
 const regionsel = document.querySelector("#region-select");
+const area = document.querySelector(".region-select");
+const checkIcon = document.querySelector(".check-icon.area");
 
 checkID = () => {
   const icon = userid.nextElementSibling;
@@ -36,7 +38,7 @@ checkPasswd = () => {
     return false;
   } else if (passwd.value.length < 12) {
     errorMsgPasswd.innerText = "비밀번호는 12자리 이상 입력하세요";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else {
     errorMsgPasswd.innerText = "";
@@ -49,11 +51,11 @@ checkConfirmPasswd = () => {
   const icon = confirmPasswd.nextElementSibling;
   if (confirmPasswd.value === null || confirmPasswd.value === "") {
     errorMsgCheckPasswd.innerText = "확인 비밀번호를 입력하세요";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else if (confirmPasswd.value !== passwd.value) {
     errorMsgCheckPasswd.innerText = "비밀번호가 일치하지 않습니다";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else {
     errorMsgCheckPasswd.innerText = "";
@@ -66,11 +68,11 @@ checkEmail = () => {
   const icon = email.nextElementSibling;
   if (email.value === null || email.value === "") {
     errorMsgEmil.innerText = "이메일을 입력하세요.";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else if (!isEmailCheck(email.value)) {
     errorMsgEmil.innerText = "유효하지 않은 형식의 이메일 입니다.";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else {
     errorMsgEmil.innerText = "";
@@ -88,11 +90,11 @@ checkTel = () => {
   const icon = tel.nextElementSibling;
   if (tel.value === null || tel.value === "") {
     errorMsgTel.innerText = "전화번호를 입력해주세요.";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else if (!isTelCheck(tel.value)) {
     errorMsgTel.innerText = "유효하지 않은 형식의 전화번호 입니다.";
-    icon.classList.add("change-erroer");
+    icon.classList.add("change-error");
     return false;
   } else {
     errorMsgTel.innerText = "";
@@ -106,41 +108,31 @@ isTelCheck = (tel) => {
   return telRequire.test(tel);
 };
 
-// checkGender = () => {
-//   const radioBtns = document.querySelectorAll('input[name="gender"]');
-//   console.log(radioBtns);
-//   let selectedGender =null;
+checkGender = () => {
+  const radioBtns = document.querySelectorAll('input[name="gender"]');
+  const checkIcon = document.querySelector(".check-icon.gender");
 
-//   for (const radioBtn of radioBtns) {
-//     console.log('hhh');
-//     if(radioBtn.checked) selectedGender = radioBtn.value;
+  // 체크되었는지 여부 : 초기값은 false (체크되지 않았음)
+  let isChecked = false;
 
-//   }
+  // 라디오버튼들(남자, 여자 2개) 모두 확인
+  for (const radioBtn of radioBtns) {
+    console.log(radioBtn.value);
+    if (radioBtn.checked) isChecked = true; // 체크된게 있으면 isChecked를 true로 바꿔줌
+  }
 
-//   console.log(selectedGender);
-
-// }
-
-checkForm = (event) => {
-  event.preventDefault();
-
-  // checkID();  // T or F
-  // checkPasswd();  // T or F
-  // checkConfirmPasswd(); // T or F
-  // checkEmail();     // T or F
-  // checkTel();       // T or F
-
-  if (
-    checkID() &&
-    checkPasswd() &&
-    checkConfirmPasswd() &&
-    checkEmail() &&
-    checkTel()
-  ) {
-    location.href = "login.html";
+  /**
+   * 남자,여자 둘다 선택이 안된경우 : isChecked 변수에 false가 들어가있음 => change-error 클래스를 붙혀서 빨갛게!
+   * 둘중 하나가 선택된 경우 : isChecked 변수에 true가 들어가있음 => change-true 클래스를 붙혀서 초록색!
+   */
+  if (isChecked) {
+    checkIcon.classList.add("change-correct");
+    return true;
+  } else {
+    checkIcon.classList.add("change-error");
+    return false;
   }
 };
-btn.addEventListener("click", checkForm);
 
 selectValidate = () => {
   //alert("ok");
@@ -156,6 +148,88 @@ selectValidate = () => {
     });
   }
 };
+
+checkArea = () => {
+  const areaValue = area.value;
+
+  // 아래 변수를 직접 테스트해보세요!
+  // console.log(area);  // select 태그를 javascript 변수로 가져오기
+  // console.log(area.value);  // 사용자가 선택한 지역의 value ex) seoul
+  // console.log(area.options[area.selectedIndex].text); //사용자가 선택한 지역의 text값 ex)서울특별시
+
+  //1. 선택된 값이 없다면 ~
+  if (areaValue === "" || areaValue === null) {
+    checkIcon.classList.add("change-error");
+    return false;
+  } else {
+    checkIcon.classList.add("change-correct");
+    return true;
+  }
+};
+
+// change된 글자가 아래로 내려감. 옆으로 옮기기
+
+checkHobby = () => {
+  const checkBoxes = document.querySelectorAll(".hobby-check");
+  const checkIcon = document.querySelector(".check-icon.hobby");
+
+  let count = 0;
+  const AT_LEAST_COUNT = 1; //선택해야하는 숫자(설정값)
+
+  for (const checkbox of checkBoxes) {
+    if (checkbox.checked) count++;
+  }
+
+  if (count < AT_LEAST_COUNT) {
+    checkIcon.classList.add("change-error");
+    return false;
+  } else {
+    checkIcon.classList.add("change-correct");
+    return true;
+  }
+};
+
+checkForm = (event) => {
+  event.preventDefault();
+
+  // checkID();  // T or F
+  // checkPasswd();  // T or F
+  // checkConfirmPasswd(); // T or F
+  // checkEmail();     // T or F
+  // checkTel();       // T or F
+  // checkGender();
+  // checkArea();
+  // checkHobby();
+
+  if (
+    checkID() &&
+    checkPasswd() &&
+    checkConfirmPasswd() &&
+    checkEmail() &&
+    checkTel() &&
+    checkGender() &&
+    checkArea() &&
+    checkHobby()
+  ) {
+    location.href = "login.html";
+  }
+};
+btn.addEventListener("click", checkForm);
+
+/**
+ * 셀렉트 박스를 바꿨을때 옆에 텍스트도 같이 변경하는 코드
+ */
+onChange = () => {
+  const regionMsg = document.querySelector(".region-message");
+  const areaName = area.options[area.selectedIndex].text;
+  const areaValue = area.value;
+
+  regionMsg.innerText = areaName;
+  areaValue === null || areaValue === ""
+    ? checkIcon.classList.add("change-error")
+    : checkIcon.classList.add("change-correct");
+};
+area.addEventListener("change", onChange);
 
 // document.getElementById("frm").addEventListener("submit", (e) => {
 //   e.preventDefault(); // 페이지 전환 X
