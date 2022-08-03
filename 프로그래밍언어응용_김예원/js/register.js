a45995bb485697e7531a59f0222defd7a8ca90b1
iamyewon <101702107+iamyewon@users.noreply.github.com>
2022-08-03 10:54:15 +0900

    8.3
    am 10:54

--- 프로그래밍언어응용_김예원/js/register.js
+++ 프로그래밍언어응용_김예원/js/register.js
@@ -1,95 +1,110 @@
-const userid = document.querySelector('#userid');
-const errorMsgID = document.querySelector('.errorMsg-id');
-const passwd = document.querySelector('#passwd');
-const errorMsgPasswd = document.querySelector('.errorMsg-passwd');
-const confirmPasswd = document.querySelector('#confirm-passwd');
-const errorMsgCheckPasswd = document.querySelector('.errorMsg-check-passwd');
-const email = document.querySelector('#email');
-const errorMsgEmil = document.querySelector('.errorMsg-email');
-const tel = document.querySelector('#tel');
-const errorMsgTel = document.querySelector('.errorMsg-tel');
-const btn = document.querySelector('#signup-btn');
-const gender = document.querySelector('.genderRadio');
+const userid = document.querySelector("#userid");
+const errorMsgID = document.querySelector(".errorMsg-id");
+const passwd = document.querySelector("#passwd");
+const errorMsgPasswd = document.querySelector(".errorMsg-passwd");
+const confirmPasswd = document.querySelector("#confirm-passwd");
+const errorMsgCheckPasswd = document.querySelector(".errorMsg-check-passwd");
+const email = document.querySelector("#email");
+const errorMsgEmil = document.querySelector(".errorMsg-email");
+const tel = document.querySelector("#tel");
+const errorMsgTel = document.querySelector(".errorMsg-tel");
+const btn = document.querySelector("#signup-btn");
+const regionsel = document.querySelector("#region-select");
 
-checkID = (event) => { 
-  const icon = userid.nextElementSibling; 
-  if(userid.value === null || userid.value === ''){
-    errorMsgID.innerText = '아이디를 입력하세요.';
-    icon.classList.add('change-error');    
+checkID = () => {
+  const icon = userid.nextElementSibling;
+  if (userid.value === null || userid.value === "") {
+    errorMsgID.innerText = "아이디를 입력하세요.";
+    icon.classList.add("change-error");
     return false;
-  }else if(userid.value.length < 9 ){
-    errorMsgID.innerText = '아이디는 9자리 이상 입력하세요.';    
-    icon.classList.add('change-error');
-    return false;
-  }else {
-    errorMsgID.innerText = '';
-    icon.classList.add('change-correct');
-    return true;
-  }
-}
-// 초록빨강
-checkPasswd = () => {
-  if(passwd.value === null || passwd.value === ''){
-    errorMsgPasswd.innerText = '비밀번호를 입력하세요';
-    return false;
-  }
-  else if(passwd.value.length < 12 ){
-    errorMsgPasswd.innerText = '비밀번호는 12자리 이상 입력하세요';
-    return false;
-  }else{
-    errorMsgPasswd.innerText = '';
-    return true;
-  }
-}
-
-checkConfirmPasswd = () => {
-  if(passwd.value === null || passwd.value === ''){
-    errorMsgCheckPasswd.innerText = '확인 비밀번호를 입력하세요'
-    return false;
-  }else if(confirmPasswd.value !== passwd.value){
-    errorMsgCheckPasswd.innerText = '비밀번호가 일치하지 않습니다';
-    return false;
-  }else{
-    errorMsgCheckPasswd.innerText = '';
-    return true;
-  }
-}
-
-checkEmail = () => {
-  if (email.value === null || email.value === '') {
-    errorMsgEmil.innerText = '이메일을 입력하세요.';
-    return false;
-  } else if (!isEmailCheck(email.value)) {
-    errorMsgEmil.innerText = '유효하지 않은 형식의 이메일 입니다.';
+  } else if (userid.value.length < 9) {
+    errorMsgID.innerText = "아이디는 9자리 이상 입력하세요.";
+    icon.classList.add("change-error");
     return false;
   } else {
-    errorMsgEmil.innerText = '';
+    errorMsgID.innerText = "";
+    icon.classList.add("change-correct");
     return true;
   }
-} 
+};
+// 초록빨강
+checkPasswd = () => {
+  const icon = passwd.nextElementSibling;
+  if (passwd.value === null || passwd.value === "") {
+    errorMsgPasswd.innerText = "비밀번호를 입력하세요";
+    icon.classList.add("change-error");
+    return false;
+  } else if (passwd.value.length < 12) {
+    errorMsgPasswd.innerText = "비밀번호는 12자리 이상 입력하세요";
+    icon.classList.add("change-erroer");
+    return false;
+  } else {
+    errorMsgPasswd.innerText = "";
+    icon.classList.add("change-correct");
+    return true;
+  }
+};
+
+checkConfirmPasswd = () => {
+  const icon = confirmPasswd.nextElementSibling;
+  if (confirmPasswd.value === null || confirmPasswd.value === "") {
+    errorMsgCheckPasswd.innerText = "확인 비밀번호를 입력하세요";
+    icon.classList.add("change-erroer");
+    return false;
+  } else if (confirmPasswd.value !== passwd.value) {
+    errorMsgCheckPasswd.innerText = "비밀번호가 일치하지 않습니다";
+    icon.classList.add("change-erroer");
+    return false;
+  } else {
+    errorMsgCheckPasswd.innerText = "";
+    icon.classList.add("change-correct");
+    return true;
+  }
+};
+
+checkEmail = () => {
+  const icon = email.nextElementSibling;
+  if (email.value === null || email.value === "") {
+    errorMsgEmil.innerText = "이메일을 입력하세요.";
+    icon.classList.add("change-erroer");
+    return false;
+  } else if (!isEmailCheck(email.value)) {
+    errorMsgEmil.innerText = "유효하지 않은 형식의 이메일 입니다.";
+    icon.classList.add("change-erroer");
+    return false;
+  } else {
+    errorMsgEmil.innerText = "";
+    icon.classList.add("change-correct");
+    return true;
+  }
+};
 
 isEmailCheck = (email) => {
   let emailRequire = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
   return emailRequire.test(email);
-}
+};
 
 checkTel = () => {
-  if(tel.value === null || tel.value === ''){
-    errorMsgTel.innerText = '전화번호를 입력해주세요.';
+  const icon = tel.nextElementSibling;
+  if (tel.value === null || tel.value === "") {
+    errorMsgTel.innerText = "전화번호를 입력해주세요.";
+    icon.classList.add("change-erroer");
     return false;
-  }else if (!isTelCheck(tel.value)){
-    errorMsgTel.innerText = '유효하지 않은 형식의 전화번호 입니다.';
+  } else if (!isTelCheck(tel.value)) {
+    errorMsgTel.innerText = "유효하지 않은 형식의 전화번호 입니다.";
+    icon.classList.add("change-erroer");
     return false;
-  }else{
-    errorMsgTel.innerText = '';
+  } else {
+    errorMsgTel.innerText = "";
+    icon.classList.add("change-correct");
     return true;
   }
-}
+};
 
 isTelCheck = (tel) => {
   let telRequire = /^[0-9+-\_.]+$/;
   return telRequire.test(tel);
-}
+};
 
 // checkGender = () => {
 //   const radioBtns = document.querySelectorAll('input[name="gender"]');
@@ -99,34 +114,58 @@ isTelCheck = (tel) => {
 //   for (const radioBtn of radioBtns) {
 //     console.log('hhh');
 //     if(radioBtn.checked) selectedGender = radioBtn.value;
-    
+
 //   }
 
 //   console.log(selectedGender);
 
 // }
 
-
-
 checkForm = (event) => {
   event.preventDefault();
-  
+
   // checkID();  // T or F
   // checkPasswd();  // T or F
   // checkConfirmPasswd(); // T or F
   // checkEmail();     // T or F
   // checkTel();       // T or F
 
-  if(checkID() && checkPasswd() && checkConfirmPasswd() && checkEmail() && checkTel()){
-    location.href = 'login.html';
-  };  
-}
-btn.addEventListener('click', checkForm);
-  
-  const textarea = document.querySelector('#introduction');
-  const p = document.querySelector(".input-cnt");
-  textarea.addEventListener("keyup", (e) => {
-    //console.log(e);
-    p.textContent = `입력한 글자 수 : ${e.target.value.length}`;
-  });
+  if (
+    checkID() &&
+    checkPasswd() &&
+    checkConfirmPasswd() &&
+    checkEmail() &&
+    checkTel()
+  ) {
+    location.href = "login.html";
+  }
+};
+btn.addEventListener("click", checkForm);
 
+selectValidate = () => {
+  //alert("ok");
+  if (regionsel.value === "") {
+    alert("검색타입을 선택하세요");
+    return false;
+  } else {
+    regionsel.addEventListener("change", (e) => {
+      const option = e.qurrentTarget.options;
+      const idx = e.currentTarget.options.selectedIndex;
+      const message = document.querySelector("#region-message");
+      message.textContent = `${options[idx].textContent}`;
+    });
+  }
+};
+
+// document.getElementById("frm").addEventListener("submit", (e) => {
+//   e.preventDefault(); // 페이지 전환 X
+//   selectValidate();
+//   // 함수만 checkForm 쪽으로 올려주면 될듯?
+// });
+
+const textarea = document.querySelector("#introduction");
+const p = document.querySelector(".input-cnt");
+textarea.addEventListener("keyup", (e) => {
+  //console.log(e);
+  p.textContent = `${e.target.value.length} 글자 입니다.`;
+});
